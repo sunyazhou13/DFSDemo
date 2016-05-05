904bc165c098128bbb656170b4952fc6608ec859
sunyazhou <sunyazhou@baidu.com>
2016-05-05 16:56:27 +0800

    增加文件深度优化
    文件深度优化

--- DFSDemo/AppDelegate.m
+++ DFSDemo/AppDelegate.m
@@ -8,6 +8,7 @@
 
 #import "AppDelegate.h"
 
+
 @interface AppDelegate ()
 
 @property (weak) IBOutlet NSWindow *window;
@@ -43,41 +44,74 @@
 
 - (void)dfsUrls:(NSArray *)urls
 {
-    NSLog(@"%@",urls);
-    if (urls.count == 0) { return; }
     
-    
-    
-    //深度遍历
-    NSFileManager *fileManager = [NSFileManager defaultManager];
-    NSArray *keys = [NSArray arrayWithObject:NSURLIsRegularFileKey];
-    
-    for (NSURL *localUrl in urls) {
-        NSDirectoryEnumerator *enumerator = [fileManager
-                                             enumeratorAtURL:localUrl
-                                             includingPropertiesForKeys:keys 
-                                             options:0
-                                             errorHandler:^(NSURL *url, NSError *error) {
-                                                 // Handle the error.
-                                                 // Return YES if the enumeration should continue after the error.
-                                                 return YES;
-                                             }];
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        NSLog(@"所有URLs%@",urls);
+        if (urls.count == 0) { return; }
         
-        for (NSURL *url in enumerator) {
-            NSError *error;
-            NSNumber *isDirectory = nil;
-            if (! [url getResourceValue:&isDirectory forKey:NSURLIsDirectoryKey error:&error]) {
-                // handle error
-            }
-            else if (! [isDirectory boolValue]) {
-                // No error and it’s not a directory; do something with the file
-            }
+        NSTimeInterval currentTime = [[NSDate date] timeIntervalSince1970];
+        
+        
+        
+        //深度遍历
+        NSFileManager *fileManager = [NSFileManager defaultManager];
+        NSMutableArray *urlDirFiles = [[NSMutableArray alloc] initWithCapacity:0];
+        NSArray *keys = [NSArray arrayWithObjects:NSURLIsDirectoryKey,NSURLParentDirectoryURLKey, nil];
+        for (NSURL *localUrl in urls) {
+            NSDirectoryEnumerator *enumerator = [self enumeratorPathByFileManager:fileManager atURL:localUrl propertiesForKeys:keys options:0];
             
-            NSLog(@"%@", url);
+            //这里包含的元素是 有子文件的忽略父路径结点
+            //eg: /A/1/2/ (这个就需要移除)   /A/1/2/sun.txt(保留这个文件即可）
+            for (NSURL *url in enumerator) {
+                NSError *error;
+                NSNumber *isDirectory = nil;
+                if (! [url getResourceValue:&isDirectory forKey:NSURLIsDirectoryKey error:&error]) {
+                    // handle error
+                }
+                else if (! [isDirectory boolValue]) {
+                    // No error and it’s not a directory; do something with the file
+                }
+                
+                //是否为文件夹
+                if ([isDirectory boolValue]) {
+                    NSDirectoryEnumerator *dirEnumerator = [self enumeratorPathByFileManager:fileManager atURL:url propertiesForKeys:keys options:NSDirectoryEnumerationSkipsSubdirectoryDescendants];
+                    if (dirEnumerator.allObjects.count > 0) {
+                        NSLog(@"文件夹内有文件,忽略此条路径 %@",[url path]);
+                    } else {
+                        [urlDirFiles addObject:[url path]];
+                        
+                    }
+                } else {
+                    [urlDirFiles addObject:[url path]];
+                }
+            }
+            NSLog(@"所有可上传文件列表:\n%@",urlDirFiles);
         }
-    }
-    
-    
-    
+
+        dispatch_async(dispatch_get_main_queue(), ^{
+            NSTimeInterval nowTime = [[NSDate date] timeIntervalSince1970];
+            NSLog(@"\n文件数量:%zd\n耗时:%.2f 秒",urlDirFiles.count,(nowTime - currentTime));
+        });
+    });
 }
+
+- (NSDirectoryEnumerator *)enumeratorPathByFileManager:(NSFileManager *)fileManager
+                                                 atURL:(NSURL *)url
+                                     propertiesForKeys:(nullable NSArray<NSString *> *)keys
+                                               options:(NSDirectoryEnumerationOptions)mask
+{
+    NSDirectoryEnumerator *enumerator = [fileManager
+                                         enumeratorAtURL:url
+                                         includingPropertiesForKeys:keys
+                                         options:mask
+                                         errorHandler:^(NSURL *url, NSError *error) {
+                                             // Handle the error.
+                                             // Return YES if the enumeration should continue after the error.
+                                             NSLog(@"深度遍历出错%@",error);
+                                             return YES;
+                                         }];
+    return enumerator;
+}
+
+
 @end
